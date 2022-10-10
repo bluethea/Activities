bdbb649771485f4e973b76c23185bb50791bf388
bluethea <nazar.lukiashko@knf.stud.vu.lt>
2022-10-10 11:29:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Add Note", "Delete Note"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.list_menu);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                // TODO Auto-generated method stub
+
+                Log.v("long clicked","pos: " + pos);
+
+                return true;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                Intent i;
+                if(pos == 0) {
+                    i = new Intent(getApplicationContext(), AddNoteActivity.class);
+                }
+                else {
+                    i = new Intent(getApplicationContext(), DeleteNoteActivity.class);
+                }
+                startActivity(i);
+            }
+        });
+    }
+}
